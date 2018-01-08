503ad388c83e6c171e387f79af95b05803be967f
Franciszek Andruszkiewicz <franciszek.andruszkiewicz@crafton.pl>
2018-01-08 07:56:39 +0100

    change files

--- src/js/_roll.js
+++ src/js/_roll.js
@@ -1,11 +1,18 @@
+var wrapper = document.getElementsByClassName('columns')[0];
+var col1 = document.getElementsByClassName('columns__col1')[0];
+var col2 = document.getElementsByClassName('columns__col2')[0];
+var items = col2.getElementsByClassName('content').length;
 
-var col2 = document.getElementsByClassName('content')[0];
-var st = 0;
+window.addEventListener('DOMContentLoaded', function() {
+  col2.style = "top: -" + (items-1) * 100 + "vh";
+  console.log('yay!');
+}, false);
 
-window.addEventListener('scroll', function(){
-  st = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0
-  
+
+wrapper.addEventListener('scroll', function(){
+  var s = wrapper.pageYOffset || wrapper.scrollTop || 0; //sets scrolltop :)
+  col2.style = "top: -" + (items-1) * 100 + "vh;transform:translate(0," +(s*2)
+   + "px);" //this sets top position of the element; above is an "if" statement in RegExp
 });
 
-console.log(st);
-  col2.style.transform = "translate(0, " + st + ")";
+//TODO: problem is in scrollbar; when col2 reach a "top = 0" and it up, then scrollbar makes smaller :/
